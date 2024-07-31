d61c84c14227bf6dd9a7b786756aa688b6dae14c
TrafimLaputski <laputskitrafim@gmail.com>
2024-07-31 12:40:23 +0200

    Joystick Create

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler
+{
+    public Action<Vector2> InputAction;
+
+    [SerializeField] private RectTransform _handler = null;
+
+    private RectTransform _backGround = null;
+
+    private Vector2 _radius = Vector2.zero;
+    private Vector2 _input = Vector2.zero;
+
+    private void Start()
+    {
+        JoystickAssembly();
+    }
+
+    private void JoystickAssembly()
+    {
+        _backGround = gameObject.GetComponent<RectTransform>();
+
+        _radius = new Vector2(_backGround.rect.width / 2, _backGround.rect.height / 2);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _input = Vector2.zero;
+        _handler.localPosition = Vector2.zero;
+        InputAction?.Invoke(Vector2.zero);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        float x = (eventData.position.x - transform.position.x) / _radius.x;
+        float y = (eventData.position.y - transform.position.y) / _radius.y;
+
+        _input.x = x;
+        _input.y = y;
+
+        if (_input.magnitude > 1)
+        {
+            _input = _input.normalized;
+        }
+
+        _handler.localPosition = _input * _radius;
+
+        InputAction?.Invoke(_input);
+    }
+}
